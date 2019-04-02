23ef9477e2194a37f5e1783e5c9d22e92acc9214
Jon Hays <mightyfancy@gmail.com>
2019-04-02 01:36:40 -0700

    made entropy algorithm much more efficient -- implemented segmenter

--- classifiers.py
+++ classifiers.py
@@ -10,38 +10,38 @@ import random
 
 class Node:
 
-    def __init__(self, left, right, label, is_leaf=False, split_rule=None):
+    def __init__(self, left=None, right=None, label=None, is_leaf=False, 
+                 split_rule=None, X=None, y=None):
         self.left = left
         self.right = right
         self.label = label
         self.is_leaf = is_leaf
         self.split_rule = split_rule
+        self.X = X
+        self.y = y
 
 
 
 class DecisionTree:
 
     def __init__(self):
-        self.root = Node()
+        self.root = Node(is_leaf=True)
 
     @staticmethod
     def entropy(y):
         """
         Calculates the entropy given all the labels
         """
-        classes = {}
-        for c in y:
-            if classes.get(c):
-                classes[c] += 1
-            else:
-                classes[c] = 1
-        entropy = 0
         n = len(y)
-        for c in classes.keys():
-            p = classes[c] / n
-            surprise = -np.log2(p)
-            entropy += p * surprise
+        if n <= 1:
+            return 0
+        _, counts = np.unique(y, return_counts=True)
+        priors = counts / n
+        entropy = 0
+        for p in priors:
+            entropy -= p * np.log2(p)
         return entropy
+        
 
     @staticmethod
     def information_gain(X, y, thresh):
@@ -54,46 +54,82 @@ class DecisionTree:
         Sl = [y[i] for i in range(len(y)) if X[i] < thresh]
         Sr = [y[i] for i in range(len(y)) if X[i] >= thresh]
         H_after = ((len(Sl) * DecisionTree.entropy(Sl) + len(Sr) * DecisionTree.entropy(Sr)) 
-                    / (len(Sl) + len(Sr)))
+                    / len(y))
+                   
+#                    (len(Sl) + len(Sr)))
         
         return H - H_after
 
     @staticmethod
     def gini_impurity(y):
         """
-        TODO: implement a method that calculates the gini impurity given all the labels
+        Calculates the gini impurity given all the labels
         """
-        return 0
+        n = len(y)
+        if n <= 1:
+            return 0
+        _, counts = np.unique(y, return_counts=True)
+        priors = counts / n
+        summation = 0
+        for p in priors:
+            summation += p * p
+        return 1 - summation
 
     @staticmethod
     def gini_purification(X, y, thresh):
         """
-        TODO: implement a method that calculates reduction in impurity gain given a vector of features
+        Calculates reduction in impurity gain given a vector of features
         and a split threshold
         """
-        return 0
+        I = DecisionTree.entropy(y)
+        
+        Sl = [y[i] for i in range(len(y)) if X[i] < thresh]
+        Sr = [y[i] for i in range(len(y)) if X[i] >= thresh]
+        I_after = ((len(Sl) * DecisionTree.gini_impurity(Sl) + len(Sr) * DecisionTree.gini_impurity(Sr)) 
+                    / len(y))
+                   
+#         (len(Sl) + len(Sr)))
+        
+        return I - I_after
 
     def split(self, X, y, idx, thresh):
         """
-        TODO: implement a method that return a split of the dataset given an index of the feature and
+        Returns a split of the dataset given an index of the feature and
         a threshold for it
         """
-        return 0
+        l_indcs = [j for j in range(len(y)) if X[:,idx][j] < thresh]
+        r_indcs = [j for j in range(len(y)) if X[:,idx][j] >= thresh]
+        return X[l_indcs], y[l_indcs], X[r_indcs], y[r_indcs]
     
-    def segmenter(self, X, y):
+    def segmenter(self, X, y, entropy_gain=DecisionTree.information_gain):
         """
-        TODO: compute entropy gain for all single-dimension splits,
+        Computes entropy gain for all single-dimension splits,
         return the feature and the threshold for the split that
         has maximum gain
         """
-        return 0
+        # Initialize split rule arbitrarily
+        split_idx = X.T[0]
+        split_thresh = X.T[0][0]
+        max_gain = 0
+        # Iterate through each feature
+        for i, x in enumerate(X.T):
+            # Iterate through each unique feature value
+            thresh_vals = np.unique(x)
+            for thresh in thresh_vals:
+                gain = entropy_gain(x, y, thresh)
+                if gain > max_gain:
+                    max_gain = gain
+                    split_idx = i
+                    split_thresh = thresh
+        return split_idx, split_thresh
     
     
     def fit(self, X, y):
         """
-        TODO: fit the model to a training set. Think about what would be 
-        your stopping criteria
+        fits the model to a training set. ...stopping criteria
         """
+        
+        # clear tree first
         return 0
 
     def predict(self, X):
